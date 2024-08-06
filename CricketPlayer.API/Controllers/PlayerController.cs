a8682c2845376c92d646bfee1c93528d7fc934b5
premkumar sundaramoorthy <premgg37@gmail.com>
2024-08-06 18:42:34 +0530

    Implemented CRUD APIs

---
+++ CricketPlayer.API/Controllers/PlayerController.cs
@@ -0,0 +1,101 @@
+﻿using CricketPlayer.API.Data;
+using CricketPlayer.API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CricketPlayer.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlayerController : ControllerBase
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public PlayerController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        [Route("GetAll")]
+        public IActionResult GetAll()
+        {
+            List<Player> players = _dbContext.Player.ToList();
+
+            return Ok(players);
+        }
+
+        [HttpGet]
+        [Route("GetById")]
+        public IActionResult GetById(int Id)
+        {
+            Player player = _dbContext.Player.Where(x => x.Id == Id).FirstOrDefault();
+
+            if (player == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(player);
+        }
+
+        [HttpGet]
+        [Route("GetOrderedListByRank")]
+        public IActionResult GetOrderedListByRank()
+        {
+            List<Player> players = _dbContext.Player.OrderBy(x => x.Rank).ToList();
+
+            return Ok(players);
+        }
+
+        [HttpPost]
+        [Route("Create")]
+        public IActionResult Create(Player player)
+        {
+            _dbContext.Player.Add(player);
+
+            _dbContext.SaveChanges();
+
+            return NoContent();
+        }
+
+
+        [HttpPut]
+        [Route("Update")]
+        public IActionResult Update(Player _player)
+        {
+            Player player = _dbContext.Player.AsNoTracking().Where(x => x.Id == _player.Id).FirstOrDefault();
+
+            if (player == null)
+            {
+                return NotFound();
+            }
+
+            _dbContext.Player.Update(_player);
+
+            _dbContext.SaveChanges();
+
+            return NoContent();
+        }
+
+
+        [HttpDelete]
+        [Route("Delete")]
+        public IActionResult Delete(int Id)
+        {
+            Player player = _dbContext.Player.Where(x => x.Id == Id).FirstOrDefault();
+
+            if (player == null)
+            {
+                return NotFound();
+            }
+
+            _dbContext.Player.Remove(player);
+
+            _dbContext.SaveChanges();
+
+            return NoContent();
+        }
+    }
+}
