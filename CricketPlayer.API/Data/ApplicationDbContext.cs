b87eb851ae8e0a83d91f9698cba593f7fe081203
premkumar sundaramoorthy <premgg37@gmail.com>
2024-08-06 13:16:49 +0530

    Created Application DbContext

---
+++ CricketPlayer.API/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CricketPlayer.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CricketPlayer.API.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+            
+        }
+
+        DbSet<Player> Player { get; set; }
+    }
+}
