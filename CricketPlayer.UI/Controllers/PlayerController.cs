3336f258b3777a117c666ddefb846fc7a26ea2c6
premkumar sundaramoorthy <premgg37@gmail.com>
2024-08-06 19:59:17 +0530

    Get API integration in UI

--- CricketPlayer.UI/Controllers/PlayerController.cs
+++ CricketPlayer.UI/Controllers/PlayerController.cs
@@ -1,13 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CricketPlayer.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System.Diagnostics;
 
 namespace CricketPlayer.UI.Controllers
 {
     public class PlayerController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        private HttpClient _httpClient;
+
+        private static string BaseUrl = "http://localhost:5255";
+
+        public PlayerController(IHttpClientFactory httpClientFactory)
         {
-            return View();
+            _httpClientFactory = httpClientFactory;
+            _httpClient = _httpClientFactory.CreateClient();
+        }
+        public async Task<IActionResult> Index()
+        {
+            var response = await _httpClient.GetAsync($"{BaseUrl}/api/Player/GetAll");
+
+            var jsonContent = await response.Content.ReadAsStringAsync();
+
+            List<Player> players = JsonConvert.DeserializeObject<List<Player>>(jsonContent);
+
+            return View(players);
         }
     }
 }
